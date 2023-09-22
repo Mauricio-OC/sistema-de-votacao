15d66e6aca9c5c8d141fb629da67c972d873eeef
Mauricio-OC <murisc0@hotmail.com>
2023-09-22 17:05:52 -0300

    req 02

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,6 +1,37 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private  int votos;
 
+  public PessoaCandidata(String nome, int numero) {
+    super.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
 
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+  public  int getVotos() {
+    return votos;
+  }
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    votos++;
+  }
 }
