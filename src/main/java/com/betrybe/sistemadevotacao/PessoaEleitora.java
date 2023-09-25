3387df9030f98152cdcfa9494decf494f57fcfbb
Mauricio-OC <murisc0@hotmail.com>
2023-09-25 11:46:05 -0300

    req 03 pessoa eleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,30 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * PessoaEleitora.
+ */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return  nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    super.setNome(nome);
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
