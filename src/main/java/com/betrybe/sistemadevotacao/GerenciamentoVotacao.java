28f600cd884c445ec0948382adfad09c7525a96e
Mauricio-OC <murisc0@hotmail.com>
2023-09-27 17:35:37 -0300

    req 05 + 06
    05 Add metodos de cadastrar pessoas eleitoras
    06 Add metodos para verificar pessoas q ja votaram e mostrar resultados

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -22,21 +22,71 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    PessoaCandidata candidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(candidata);
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-
+    for (PessoaEleitora eleitora : pessoasEleitoras) {
+      if (eleitora.getCpf().equals(cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    PessoaEleitora eleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(eleitora);
   }
 
   @Override
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
-
+    if (cpfsComputados.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    }
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numeroPessoaCandidata) {
+        candidata.receberVoto();
+        cpfsComputados.add(cpfPessoaEleitora);
+        return;
+      }
+    }
   }
 
   @Override
   public void mostrarResultado() {
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado!");
+      return;
+    }
+    int totalVotos = cpfsComputados.size();
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      int votosRecebidos = candidata.getVotos();
+      double percentual = (double) votosRecebidos / totalVotos * 100;
+      System.out.println(
+          "Nome: " + candidata.getNome() + " - " + votosRecebidos + " votos ( "
+              +
+              Math.round(percentual) + "% )"
+      );
+    }
+    System.out.println("Total de votos: " + totalVotos);
+  }
 
+  public ArrayList<PessoaCandidata> getPessoasCandidatas() {
+    return pessoasCandidatas;
+  }
+
+  public ArrayList<PessoaEleitora> getPessoasEleitoras() {
+    return pessoasEleitoras;
+  }
+
+  public ArrayList<String> getCpfsComputados() {
+    return cpfsComputados;
   }
 }
